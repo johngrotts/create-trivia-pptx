04a9effb3ceac22fdcdf30907b9f077ae27f3823
johngrotts <johnsmaillist@gmail.com>
2025-02-11 22:25:07 -0600

    Fixing bugs

--- src/app/create-trivia-ppt/create-trivia-ppt.component.ts
+++ src/app/create-trivia-ppt/create-trivia-ppt.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-create-trivia-ppt',
   imports: [],
+  providers: [
+    BrowserModule,
+  ],
   templateUrl: './create-trivia-ppt.component.html',
   styleUrl: './create-trivia-ppt.component.scss'
 })
-export class CreateTriviaPptComponent {
+export class CreateTriviaPptComponent implements OnInit {
 
+  constructor(protected http: HttpClient, ){}
+  
+  public ngOnInit(): void {
+    this.http.get('../../assets/BasicAllQ1/docProps/core.xml', { responseType: 'text' })
+      .pipe(
+        switchMap(async xml => await console.log('FILE: ', xml))
+      );
+  }
 }
